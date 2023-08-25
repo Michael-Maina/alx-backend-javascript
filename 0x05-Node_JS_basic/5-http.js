5986181576cead04699aaf792831539ff9f8fcd1
Michael-Maina <michaelmaina02@gmail.com>
2023-08-25 10:13:38 +0300

    Added tasks 2 - 7

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const students = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+      res.write('This is a list of our students\n');
+      students(process.argv[2]).then((data) => {
+        res.write(`Number of students: ${data.students.length}\n`);
+        res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+        res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+        res.end();
+      }).catch((error) => {
+        res.end(error.message);
+      })
+  }
+});
+
+app.listen(1245, '127.0.0.1');
+
+module.exports = app;
---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const students = require('./3-read_file_async');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is a list of our students\n');
+  await students(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+    res.end();
+  }).catch((error) => {
+    res.write(error.message);
+  })
+  .finally(() => {
+    res.end();
+  })
+});
+
+app.listen(1245, '127.0.0.1');
+
+module.exports = app;
