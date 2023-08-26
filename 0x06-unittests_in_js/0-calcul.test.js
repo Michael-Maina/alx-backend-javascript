052b8ed2f5c2b5acffde779c56d79228ce41048b
Michael-Maina <michaelmaina02@gmail.com>
2023-08-26 05:00:10 +0300

    Added test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,6 +9,8 @@ describe('calculateNumber', function() {
 
   it(`correctly adds 1 and 2`, testCalculateNumber({a: 1, b: 2, expected: 3}));
   it(`correctly adds 1.3 and 3.7`, testCalculateNumber({a: 1.3, b: 3.7, expected: 5}));
+  it(`correctly adds 1 and 3.7`, testCalculateNumber({a: 1, b: 3.7, expected: 5}));
+  it(`correctly adds 1.3 and 3`, testCalculateNumber({a: 1.3, b: 3, expected: 4}));
   it(`correctly adds 1 and -2`, testCalculateNumber({a: 1, b: -2, expected: -1}));
   it(`correctly adds 10000 and 200000`, testCalculateNumber({a: 10000, b: 200000, expected: 210000}));
   it(`correctly adds -1.3 and 3.7`, testCalculateNumber({a: -1.3, b: 3.7, expected: 3}));
