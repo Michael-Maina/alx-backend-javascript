0345aad771230f1992c7973c990d48d6690fa3f8
Michael-Maina <michaelmaina02@gmail.com>
2023-08-26 03:55:47 +0300

    Bug fixing

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,15 +2,16 @@ import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(req, res) {
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.write('This is the list of our students\n');
 
     readDatabase('./database.csv').then((data) => {
+      res.statusCode = 200;
       res.write(`Number of students in CS: ${data.CS.length}. List: ${data.CS.join(', ')}\n`);
       res.write(`Number of students in SWE: ${data.SWE.length}. List: ${data.SWE.join(', ')}\n`);
       res.end();
     }).catch((err) => {
+      res.statusCode = 500;
       res.send(err.message);
     });
   }
